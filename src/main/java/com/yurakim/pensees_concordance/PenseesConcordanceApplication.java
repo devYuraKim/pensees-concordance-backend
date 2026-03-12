c69e87d7e3ddfbbd57ecc03af09f44d63aca6f20
devYuraKim <dev.yurakim@gmail.com>
2026-03-12 15:47:48 +0900

    initial backend setup

---
+++ src/main/java/com/yurakim/pensees_concordance/PenseesConcordanceApplication.java
@@ -0,0 +1,13 @@
+package com.yurakim.pensees_concordance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenseesConcordanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenseesConcordanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurakim/pensees_concordance/PenseesConcordanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurakim.pensees_concordance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenseesConcordanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
